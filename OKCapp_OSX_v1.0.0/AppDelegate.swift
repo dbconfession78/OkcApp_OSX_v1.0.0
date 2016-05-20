474283d8dece0078bf742864026c29d713f24a6d
Stuart Kuredjian <sgkur04@gmail.com>
2016-05-20 14:57:08 -0400

    Initial Commit

---
+++ OKCapp_OSX_v1.0.0/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OKCapp_OSX_v1.0.0
+//
+//  Created by Stuart Kuredjian on 5/20/16.
+//  Copyright © 2016 s.Ticky Games. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ OKCapp_OSX_v1.0.0/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OKCapp_OSX_v1.0.0
+//
+//  Created by Stuart Kuredjian on 5/20/16.
+//  Copyright © 2016 s.Ticky Games. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
